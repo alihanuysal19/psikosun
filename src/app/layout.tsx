410a7ac43cc502945950fca150416de83a64daba
Alihan Uysal <alihanuysal169@gmail.com>
2026-04-20 21:37:10 +0300

    fix root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-
 import React from "react";
 import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
@@ -6,45 +5,37 @@ import "./css/globals.css";
 import { ThemeModeScript, ThemeProvider } from "flowbite-react";
 import customTheme from "@/utils/theme/custom-theme";
 import { CustomizerContextProvider } from "@/app/context/customizerContext";
-import '../utils/i18n';
-import NextTopLoader from 'nextjs-toploader';
-const plus_jakarta_sans = Plus_Jakarta_Sans({ subsets: ["latin"] });
-import { AuthProvider } from '../app/context/AuthContext'
+import "../utils/i18n";
+import NextTopLoader from "nextjs-toploader";
+import { AuthProvider } from "../app/context/AuthContext";
 
+const plus_jakarta_sans = Plus_Jakarta_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "NextKit - Supabase Prisma Orm",
-  description: "NextKit is a backend template powered by supabase and Prisma orm",
+  description:
+    "NextKit is a backend template powered by supabase and Prisma orm",
 };
 
 export default function RootLayout({
   children,
-  session
 }: {
   children: React.ReactNode;
-  session: any
 }) {
   return (
-
     <html lang="en">
       <head>
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <ThemeModeScript />
       </head>
-      <body className={`${plus_jakarta_sans.className}`}>
+      <body className={plus_jakarta_sans.className}>
         <NextTopLoader />
         <ThemeProvider theme={customTheme}>
           <AuthProvider>
-            <CustomizerContextProvider>
-              {children}
-            </CustomizerContextProvider>
+            <CustomizerContextProvider>{children}</CustomizerContextProvider>
           </AuthProvider>
         </ThemeProvider>
       </body>
-    </html >
-
+    </html>
   );
 }
-
-
-
